453f34c42c295e45da273e368c73b0bae092a3fc
Nick Johnson <arachnid@notdot.net>
2021-03-11 20:27:56 +1300

    Enable optimiser

--- hardhat.config.js
+++ hardhat.config.js
@@ -17,6 +17,14 @@ task("accounts", "Prints the list of accounts", async () => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.2",
+  solidity: {
+    version: "0.8.2",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 10000,
+      }
+    }
+  }
 };
 
