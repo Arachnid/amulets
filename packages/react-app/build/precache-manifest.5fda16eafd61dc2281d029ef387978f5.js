aa6feb5a79b9b1e3307ea6af3b37faf4890b52e2
Nick Johnson <arachnid@notdot.net>
2021-03-24 13:19:58 +1300

    Logos and titles

---
+++ packages/react-app/build/precache-manifest.5fda16eafd61dc2281d029ef387978f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09d8d0829fed085cacd33ec2c96d2c94",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4a56be9e2032dc5e49df",
+    "url": "./static/css/2.eaf42895.chunk.css"
+  },
+  {
+    "revision": "e95693be4106d7c02ae7",
+    "url": "./static/css/main.3f37dc0e.chunk.css"
+  },
+  {
+    "revision": "4a56be9e2032dc5e49df",
+    "url": "./static/js/2.1246d91e.chunk.js"
+  },
+  {
+    "revision": "50f80eed06b0fae588e333309a50fc2c",
+    "url": "./static/js/2.1246d91e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e95693be4106d7c02ae7",
+    "url": "./static/js/main.3f57bf8f.chunk.js"
+  },
+  {
+    "revision": "7242a8842eb86339cc63",
+    "url": "./static/js/runtime-main.c8135626.js"
+  }
+]);
