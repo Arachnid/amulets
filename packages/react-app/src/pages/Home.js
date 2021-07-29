081f19351cab684dfb548476bdd1723b2c17c582
Alex <alex.beciana@gmail.com>
2021-07-28 23:14:05 -0400

    Added fonts to body and tags

--- packages/react-app/src/pages/Home.js
+++ packages/react-app/src/pages/Home.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PageLayout from '../utils/PageLayout'
 
 import { AmuletCreator } from '../components'
-import { Row, Col, Button, Menu, Alert, Input, List, Card, Switch as SwitchD, Typography } from "antd";
+// import { Row, Col, Button, Menu, Alert, Input, List, Card, Switch as SwitchD, Typography } from "antd";
 
 const Home = (props) => {
 
@@ -10,7 +10,7 @@ const Home = (props) => {
         <PageLayout>
             <h1>Home page</h1>
             {props.address && <AmuletCreator contracts={props.contracts} provider={props.provider} />}
-              {!props.address && <Typography.Text>Connect your wallet to mint an amulet</Typography.Text>}
+              {!props.address && <div>Connect your wallet to mint an amulet</div>}
         </PageLayout>
     )
 }
